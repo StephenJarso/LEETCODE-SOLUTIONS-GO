0f6a7db80f64beff1c19da375c0589d16bf14912
stephenjarso <stephenjacob815@gmail.com>
2026-06-04 02:11:26 +0300

    feat:looping through an array to find the index of items that add upto the target

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/StephenJarso/leetcode-solutions-go
+
+go 1.22.2
---
+++ solutions/arrays/two-sum/solution.go
@@ -0,0 +1,14 @@
+package twoSum
+
+func twoSum(nums []int, target int) []int {
+    result :=[]int{}
+    for i:=0;i<len(nums);i++{
+        for j :=i+1;j<len(nums);j++{
+            if nums[i]+nums[j]==target{
+                result = append(result,i,j)
+                return result
+            }
+        }
+    }
+    return []int{}
+}
